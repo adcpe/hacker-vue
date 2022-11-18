7bd5ffe29b5eaeedd762aa43623af8346fdf3dfc
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-18 16:34:14 -0500

    Changed component name, created component files

---
+++ src/views/StoryPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { getData } from '../utils/api'
+
+// const user = ref({})
+// const route = useRoute()
+
+// getData(route.params.id, 'user').then((res) => (user.value = res))
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
