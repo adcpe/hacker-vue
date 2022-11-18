af60e23529713cc48551f0082c0336d7471100b4
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-18 16:25:33 -0500

    Added user page

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { getData } from '../utils/api'
+
+const user = ref({})
+const route = useRoute()
+
+getData(route.params.id, 'user').then((res) => (user.value = res))
+</script>
 
 <template>
-  <div></div>
+  <div class="min-h-[25vh] bg-zinc-300 p-4">
+    <div class="flex flex-row my-2">
+      <div class="font-bold w-20">User:</div>
+      <div>{{ user.id }}</div>
+    </div>
+    <div class="flex flex-row my-2">
+      <div class="font-bold w-20">Created:</div>
+      <div>{{ user.created }}</div>
+    </div>
+    <div class="flex flex-row my-2">
+      <div class="font-bold w-20">Karma:</div>
+      <div>{{ user.karma }}</div>
+    </div>
+    <div class="flex flex-row my-2">
+      <div class="font-bold w-20">About:</div>
+      <div v-html="user.about"></div>
+    </div>
+  </div>
 </template>
-
-<style lang="scss" scoped></style>
