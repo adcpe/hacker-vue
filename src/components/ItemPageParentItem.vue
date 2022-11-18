6393579a0f79333f78111650ef81f70ada838bef
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-18 17:54:21 -0500

    Finished parent item from story page

---
+++ src/components/ItemPageParentItem.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, useRoute } from 'vue-router'
+import { timePassed } from '../utils/time'
+
+const route = useRoute()
+
+console.log(route)
+const props = defineProps(['story'])
+</script>
+
+<template>
+  <div class="bg-zinc-300 px-6 py-4">
+    <h1 class="text-2xl font-bold">{{ story.title }}</h1>
+    <div class="mt-1 mb-3 opacity-50">
+      {{ story.score }} points | by
+      <RouterLink class="underline" :to="'/user/' + story.by">{{ story.by }}</RouterLink> |
+      {{ timePassed(story.time) }} ago |
+      <RouterLink class="underline" :to="route.fullPath">
+        {{ story.descendants }} comments
+      </RouterLink>
+    </div>
+    <p class="text-lg" v-html="story.text" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/ItemPage.vue
+++ src/views/ItemPage.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { getData } from '../utils/api'
+import ParentItem from '../components/ItemPageParentItem.vue'
+
+const route = useRoute()
+const story = ref({
+  by: '',
+  descendants: '',
+  id: '',
+  kids: [],
+  score: '',
+  text: '',
+  time: '',
+  title: '',
+  type: 'story'
+})
+
+getData(route.params.id, 'item').then((res) => (story.value = res))
+</script>
 
 <template>
-  <div></div>
+  <ParentItem :story="story" />
 </template>
 
 <style lang="scss" scoped></style>
