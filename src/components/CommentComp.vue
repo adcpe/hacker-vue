901e2cdfedc5b805a1f0156ca71098e584bcfd42
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-19 21:55:52 -0500

    Reduced child comments left margin

--- src/components/CommentComp.vue
+++ src/components/CommentComp.vue
@@ -22,7 +22,7 @@ getData(props.kid, 'item').then((res) => {
       {{ timePassed(comment.time) }} ago
     </div>
     <p class="mb-2 text-lg leading-relaxed" v-html="comment.text" />
-    <div class="ml-8" v-for="ck in comment.kids">
+    <div class="ml-6" v-for="ck in comment.kids">
       <Comment :kid="ck" />
     </div>
   </div>
