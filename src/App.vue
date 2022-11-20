dc2ad0c0458cd994fc5a4515c007568b44fea3fd
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-19 21:29:00 -0500

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { RouterView, useRoute } from 'vue-router'
-import NavigationComp from './components/NavigationComp.vue'
+import Navigation from './components/NavigationComp.vue'
+import Footer from './components/FooterComp.vue'
 </script>
 
 <template>
   <main class="w-10/12 xl:w-3/4 bg-zinc-200 my-0 mx-auto">
-    <NavigationComp class="flex flex-row justify-between bg-violet-500 text-zinc-200 px-6" />
+    <Navigation class="flex flex-row justify-between bg-violet-500 text-zinc-200 px-6" />
     <div class="my-1">
       <RouterView :key="useRoute().path" />
     </div>
+    <Footer />
   </main>
 </template>
 
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center py-2 bg-violet-500 text-zinc-200 px-6 text-sm">
+    <p class="my-1">
+      Made with
+      <a
+        class="text-zinc-200 visited:text-zinc-200"
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener noreferrer">
+        VueJS 3
+      </a>
+      ,
+      <a
+        class="text-zinc-200 visited:text-zinc-200"
+        href="https://tailwindcss.com/"
+        target="_blank"
+        rel="noopener noreferrer">
+        TailwindCSS
+      </a>
+      and music by Andrés Del Carpio
+    </p>
+    <p class="my-1">
+      Source code available on
+      <a
+        class="text-zinc-200 visited:text-zinc-200"
+        href="https://github.com/adcpe/hacker-vue"
+        target="_blank"
+        rel="noopener noreferrer">
+        Github
+      </a>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+a {
+  text-decoration: underline;
+}
+</style>
